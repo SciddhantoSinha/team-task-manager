b131ecfd294c91aea1f69978f667684ccadf13f1
SciddhantoSinha <sciddhantosinha@gmail.com>
2026-05-07 20:17:43 +0530

    Removed debug logs

--- config/db.js
+++ config/db.js
@@ -5,8 +5,6 @@ import pkg from "pg";
 
 const { Pool } = pkg;
 
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
