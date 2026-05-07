354a679dd5122e78921f7ed3d33db7566fc3ef92
SciddhantoSinha <sciddhantosinha@gmail.com>
2026-05-07 19:39:06 +0530

    Fixed Railway PostgreSQL config

--- config/db.js
+++ config/db.js
@@ -6,11 +6,10 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: String(process.env.DB_PASSWORD),
-  port: Number(process.env.DB_PORT),
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
